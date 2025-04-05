1e83a6c69af6c6c5676b357625b6c650bc08a268
Robert <robert.gurde@gmail.com>
2025-04-05 19:14:06 +0000

    update

--- publish.sh
+++ publish.sh
@@ -3,16 +3,18 @@
 cd dists/stable/main/binary-arm64
 dpkg-scanpackages --multiversion . > Packages
 gzip -k -f Packages
-cd ../../
-cat > Release <<EOF
+cd ../../../.. # Navigate back to the root of the repository
+
+cat > dists/stable/Release <<EOF
 Suite: stable
 Codename: stable
 Architectures: arm64
 Components: main
 EOF
-apt-ftparchive release . >> Release
-gpg --default-key "voyager@univrs.cloud" -abs -o - Release > Release.gpg
-gpg --default-key "voyager@univrs.cloud" --clearsign -o - Release > InRelease
+apt-ftparchive release dists/stable >> dists/stable/Release
+gpg --default-key "voyager@univrs.cloud" -abs -o dists/stable/Release.gpg dists/stable/Release
+gpg --default-key "voyager@univrs.cloud" --clearsign -o dists/stable/InRelease dists/stable/Release
+
 git add -A
 git commit -m update
 git push
