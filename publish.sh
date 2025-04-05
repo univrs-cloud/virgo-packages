5d91183b2f982d9649c5a75ec11569540b7aa527
Robert <robert.gurde@gmail.com>
2025-04-05 17:14:45 +0000

    update

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@
 cd dists/stable/main/binary-all
 dpkg-scanpackages --multiversion . > Packages
 gzip -k -f Packages
-cd ../../../
+cd ../../
 apt-ftparchive release . > Release
 gpg --default-key "voyager@univrs.cloud" -abs -o - Release > Release.gpg
 gpg --default-key "voyager@univrs.cloud" --clearsign -o - Release > InRelease
