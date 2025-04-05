5049ced75d0a02e40c4074d5372a340b530cd90c
Robert <robert.gurde@gmail.com>
2025-04-05 19:04:08 +0000

    update

--- publish.sh
+++ publish.sh
@@ -1,23 +1,18 @@
 #!/bin/bash -e
 
-repo_base="stable"
-arch="arm64"
-component="main"
-binary_dir="binary-$arch"
-
-cd "$repo_base/$component/$binary_dir"
+cd dists/stable/main/binary-arm64
 dpkg-scanpackages --multiversion . > Packages
 gzip -k -f Packages
-cd "../../.."
-cat > "$repo_base/Release" <<EOF
+cd ../../
+cat > Release <<EOF
 Suite: stable
 Codename: stable
-Architectures: $arch
-Components: $component
+Architectures: arm64
+Components: main
 EOF
-apt-ftparchive release "$repo_base" >> "$repo_base/Release"
-gpg --default-key "voyager@univrs.cloud" -abs -o "$repo_base/Release.gpg" "$repo_base/Release"
-gpg --default-key "voyager@univrs.cloud" --clearsign -o "$repo_base/InRelease" "$repo_base/Release"
+apt-ftparchive release . >> Release
+gpg --default-key "voyager@univrs.cloud" -abs -o - Release > Release.gpg
+gpg --default-key "voyager@univrs.cloud" --clearsign -o - Release > InRelease
 git add -A
 git commit -m update
 git push
