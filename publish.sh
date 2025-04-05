818f49ece3c50ff0698d510ab895dda40f6b4dc7
Robert <robert.gurde@gmail.com>
2025-04-05 17:39:27 +0000

    update

--- publish.sh
+++ publish.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 
-cd dists/stable/main/binary-all
+cd dists/stable/main/binary-arm64
 dpkg-scanpackages --multiversion . > Packages
 gzip -k -f Packages
 cd ../../
 cat > Release <<EOF
 Suite: stable
 Codename: stable
-Architectures: all
+Architectures: arm64
 Components: main
 EOF
 apt-ftparchive release . >> Release
